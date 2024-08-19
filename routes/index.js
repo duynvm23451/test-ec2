bf8cc5f3bc82128563c86bd9aec581e0190d02dc
duy23451 <duynvm1711@gmail.com>
2024-08-20 01:17:29 +0900

    add upload feature

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+const { PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const {
+  GetObjectCommand,
+  S3Client,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+const multer = require("multer");
 /* GET home page. */
 
 const client = new S3Client({
@@ -8,20 +16,60 @@ const client = new S3Client({
   credentials: null,
 });
 
+const dynamoDBClient = new DynamoDBClient({
+  region: "ap-northeast-1", // e.g., 'us-east-1'
+  credentials: null,
+});
+const ddbDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 router.get("/", async (req, res, next) => {
-  const filename = req.query.filename;
-  const command = new GetObjectCommand({
-    Bucket: "cloud-internship-project3-s3",
-    Key: filename,
-  });
-  try {
-    const response = await client.send(command);
-    const str = await response.Body.transformToString();
-    console.log(str);
-  } catch (err) {
-    console.log(err);
+  // const filename = req.query.filename;
+  // const command = new GetObjectCommand({
+  //   Bucket: "cloud-internship-project3-s3",
+  //   Key: filename,
+  // });
+  // try {
+  //   const response = await client.send(command);
+  //   const str = await response.Body.transformToString();
+  //   console.log(str);
+  // } catch (err) {
+  //   console.log(err);
+  // }
+  res.render("index", { bucket: "cloud-internship-project3-s3" });
+});
+
+router.post("/upload", upload.single("file"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("Không có file để upload");
   }
-  res.render("index", { title: "Express" });
+
+  const params = {
+    Bucket: "cloud-internship-project3-s3",
+    Key: req.file.originalname,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+  };
+
+  const dynamoDBParams = {
+    TableName: "S3MetadataTable",
+    Item: {
+      id: { S: req.file.originalname },
+      originalName: { S: req.file.originalname },
+      s3Uri: `s3://cloud-internship-project3-s3/${req.file.originalname}`,
+      uploadTime: { S: new Date().toISOString() },
+    },
+  };
+  try {
+    await client.send(new PutObjectCommand(params));
+    await ddbDocClient.send(new PutItemCommand(dynamoDBParams));
+    res.render("upload", { filename: req.file.originalname });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Xảy ra lỗi khi upload");
+  }
 });
 
 module.exports = router;
